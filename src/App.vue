ba85a805305e5e2afcc144386696efc2bfae1183
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-29 22:38:35 +0700

    atoms text

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { reactive } from 'vue';
 import AdButton from './components/atoms/AdButton.vue';
 import AdInput from './components/atoms/AdInput.vue';
 import AdTextArea from './components/atoms/AdTextArea.vue';
+import AdText from './components/atoms/AdText.vue';
 
 
 const testing = reactive({
@@ -12,6 +13,7 @@ const testing = reactive({
 </script>
 
 <template>
+  <AdText tag="h1">New Task</AdText>
   <AdButton type="button">Testing</AdButton>
   <AdInput placeholder="Enter something here ..." v-model="testing.inputVal" />
   <AdTextArea placeholder="Enter description here ..." v-model="testing.textAreaVal"/>
---
+++ src/components/atoms/AdText.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from "vue";
+const props = defineProps({
+    tag: {
+        type: String,
+        required: true,
+        default: "p",
+        validator: (value: string) => {
+            const _tagTypes = ["h1", "h2", "h3", "h4", "p","span","label"];
+            return _tagTypes.includes(value);
+        }
+    },
+    for: {
+        type: String,
+        required: false,
+    }
+})
+
+const getForProp = computed(()=> props.tag === 'label' ? props.for : null)
+</script>
+
+<template>
+    <component class="ad-text" :is="tag" :for="getForProp">
+
+        <slot />
+    </component>
+</template>
