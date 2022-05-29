2a4bb5c588a3bb05c52eb439be1950f2e27b2aa3
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-29 21:29:39 +0700

    atoms button

---
+++ src/components/atoms/AdButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+
+type ButtonTypes = "button" | "submit" | "reset" | undefined ;
+
+defineProps({
+    type: {
+        type: String as PropType<ButtonTypes>,
+        required: false,
+        default: 'button',
+        validators: (value: string) => {
+            const _buttonType = ["button","submit","reset"]
+            return _buttonType.includes(value);
+        },
+    }
+})
+</script>
+
+<template>
+      <button class="ad-button" :type="type">
+    
+          <slot />
+      </button>
+</template>
+
+<style>
+
+</style>
