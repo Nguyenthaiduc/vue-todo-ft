025f6b381eb3a80eaadba4e9ab0a4ffce29eec20
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-30 11:22:14 +0700

    change types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,8 @@
-export type AccordionItemsTypes  = {
-id?: string;
-_id?: string;
-title: string;
-description?: string;
-priority?: string;
-
-}
+export type AccordionItemsTypes = {
+    _id?: string;
+    title: string;
+    description: string;
+    dueDate: number;
+    priority: string;
+    completed?: boolean;
+  };
